d1ee41696a68b6dd290dc023633e23a91558963b
Michael Brown <m.overtonbrown@gmail.com>
2021-10-09 14:28:50 -0700

    hats

---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        source.Play();
+        Debug.Log("playing sound");
+    }
+}
