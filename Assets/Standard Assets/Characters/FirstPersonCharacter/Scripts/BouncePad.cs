7436afc4da933b52aad42d9c532591d75b62a34e
Michael Brown <m.overtonbrown@gmail.com>
2021-10-15 15:03:45 -0700

    basic bouncepads

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/BouncePad.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+
+#pragma warning disable 618, 649
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+    public class BouncePad : MonoBehaviour
+    {
+        public float bouncePower = 55f;
+    }
+}
