4b2a8b4831a2a2aa4309c85c4b11139e735e4c3b
Michael Brown <m.overtonbrown@gmail.com>
2021-10-15 13:47:36 -0700

    make wall morphing automatic. no player interaction anymore

--- Assets/Scripts/WallMorpher.cs
+++ Assets/Scripts/WallMorpher.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class WallMorpher : MonoBehaviour
 {
     private Vector3 originalScale;
+    // private Vector3 currentScale;
+    public float maxModulationScale = 2;
+    public float modulationSpeed = 0.1f;
+    private bool modulationScaleIncreasing = true;
+    public float modulationTime = 2f;
     private Vector3 originalPosition;
     public GameObject wallPiece;
     public float maxScaleIncreaseAmount = 2;
-    public float maxZChangeAmount = 3;
+    public float maxZChangeAmount = 3; // Unused right now.
     private float distance;
     private float colliderRadius;
 
@@ -19,23 +24,44 @@ public class WallMorpher : MonoBehaviour
         originalPosition = wallPiece.transform.position;
         SphereCollider col = GetComponent<SphereCollider>();
         colliderRadius = col.radius;
+
+        // Start each wall piece at a random point within the scale range.
+        wallPiece.transform.localScale = new Vector3(originalScale.x, Random.Range(originalScale.y, maxModulationScale), originalScale.z);
+        originalScale = wallPiece.transform.localScale;
     }
 
-    void onTriggerExit(Collider other) {
-        wallPiece.transform.localScale = originalScale;
-        // wallPiece.transform.position = originalPosition;
-    }
-
-    void OnTriggerStay(Collider other) {
-        distance = Vector3.Magnitude(transform.position - other.transform.position);
-        if(distance > colliderRadius) {
-            return;
+    void Update() {
+        if(modulationScaleIncreasing) {
+            Vector3 velocity = new Vector3(0, modulationSpeed, 0);
+            wallPiece.transform.localScale = Vector3.SmoothDamp(wallPiece.transform.localScale, new Vector3(originalScale.x, maxModulationScale, originalScale.z), ref velocity, modulationTime);
+        } else {
+            Vector3 velocity = new Vector3(0, -modulationSpeed, 0);
+            // wallPiece.transform.localScale = new Vector3(currentScale.x, currentScale.y - (Time.deltaTime * modulationSpeed), currentScale.z);
+            wallPiece.transform.localScale = Vector3.SmoothDamp(wallPiece.transform.localScale, new Vector3(1,1,1), ref velocity, modulationTime);
         }
 
-        float modPercent = 1 - Mathf.Pow((distance / colliderRadius), 1);
-        float modScaleAmount = maxScaleIncreaseAmount * modPercent;
-        float modPositionAmount = maxZChangeAmount * modPercent;
-        wallPiece.transform.localScale = new Vector3(originalScale.x, originalScale.y + modScaleAmount, originalScale.z);
-        // wallPiece.transform.position = new Vector3(originalPosition.x, originalPosition.y, originalPosition.z + modPositionAmount);
+        if(wallPiece.transform.localScale.y >= maxModulationScale) {
+            modulationScaleIncreasing = false;
+        } else if(wallPiece.transform.localScale.y <= 1) {
+            modulationScaleIncreasing = true;
+        }
     }
+
+    // void onTriggerExit(Collider other) {
+    //     wallPiece.transform.localScale = originalScale;
+    //     // wallPiece.transform.position = originalPosition;
+    // }
+
+    // void OnTriggerStay(Collider other) {
+    //     distance = Vector3.Magnitude(transform.position - other.transform.position);
+    //     if(distance > colliderRadius) {
+    //         return;
+    //     }
+
+    //     float modPercent = 1 - Mathf.Pow((distance / colliderRadius), 1);
+    //     float modScaleAmount = maxScaleIncreaseAmount * modPercent;
+    //     float modPositionAmount = maxZChangeAmount * modPercent; // Unused.
+    //     wallPiece.transform.localScale = new Vector3(originalScale.x, originalScale.y + modScaleAmount, originalScale.z);
+    //     // wallPiece.transform.position = new Vector3(originalPosition.x, originalPosition.y, originalPosition.z + modPositionAmount);
+    // }
 }
