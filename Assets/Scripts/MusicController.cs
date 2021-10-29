6cdbf2c4d96d3915a8073490676fcf7df2c19886
Michael Brown <m.overtonbrown@gmail.com>
2021-10-28 21:02:57 -0700

    track two music basics. polish in the cathedral

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MusicController : MonoBehaviour
 {
     public GameObject player;
+    public SkipController skipController;
+
+    // Track 1
     public float trackOneChangeStartY;
     public float trackOneChangeEndY;
     public AudioSource trackOne;
@@ -12,9 +15,38 @@ public class MusicController : MonoBehaviour
     private float trackOneStartFilter;
     public AudioLowPassFilter trackOneFilter;
 
+    // Track 2
+    public float trackTwoFadeInStartY;
+    public float trackTwoFadeInEndY;
+    public float trackTwoFadeOutStartY;
+    public float trackTwoFadeOutEndY;
+    public AudioSource trackTwo;
+    private float trackTwoStartVolume;
+    private float trackTwoStartFilter;
+    public AudioLowPassFilter trackTwoFilter;
+
+    // Track 2
+    public float trackThreeFadeInStartY;
+    public float trackThreeFadeInEndY;
+    public float trackThreeFadeOutStartY;
+    public float trackThreeFadeOutEndY;
+    public AudioSource trackThree;
+    private float trackThreeStartVolume;
+    private float trackThreeStartFilter;
+    public AudioLowPassFilter trackThreeFilter;
+
     void Start() {
-        trackOneStartVolume = trackOne.volume;
+        trackOneStartVolume = skipController.TrackOneMaxVolume();
         trackOneStartFilter = trackOneFilter.cutoffFrequency;
+
+        trackTwoStartVolume = skipController.TrackTwoMaxVolume();
+        trackTwoStartFilter = trackTwoFilter.cutoffFrequency;
+
+        trackThreeStartVolume = skipController.TrackThreeMaxVolume();
+        trackThreeStartFilter = trackThreeFilter.cutoffFrequency;
+
+        Debug.Log("trak one start: " + trackOneStartVolume);
+        Debug.Log("trak two start: " + trackTwoStartVolume);
     }
 
     // Update is called once per frame
@@ -28,6 +60,7 @@ public class MusicController : MonoBehaviour
             }
         }
 
+        // Fade out track 1
         if(playerYPos <= trackOneChangeStartY && playerYPos >= trackOneChangeEndY) {
             // The filter is off by default because we don't want unnecessary distortion.
             if(!trackOneFilter.enabled) {
@@ -37,7 +70,38 @@ public class MusicController : MonoBehaviour
                 ((playerYPos - trackOneChangeStartY) / (trackOneChangeEndY - trackOneChangeStartY)), 2
             );
             trackOne.volume = trackOneStartVolume * fadePercent;
-            trackOneFilter.cutoffFrequency = trackOneStartFilter * fadePercent;
+            // trackOneFilter.cutoffFrequency = trackOneStartFilter * fadePercent;
+        }
+
+        // Fade in track 2
+        if(playerYPos <= trackTwoFadeInStartY && playerYPos >= trackTwoFadeInEndY) {
+
+            float fadePercent = Mathf.Pow(
+                ((playerYPos - trackTwoFadeInStartY) / (trackTwoFadeInEndY - trackTwoFadeInStartY)), 2
+            );
+            trackTwo.volume = trackTwoStartVolume * fadePercent;
+        }
+
+        // Fade out track 2
+        if(playerYPos <= trackTwoFadeOutStartY && playerYPos >= trackTwoFadeOutEndY) {
+            // The filter is off by default because we don't want unnecessary distortion.
+            // if(!trackOneFilter.enabled) {
+            //     trackOneFilter.enabled = true;
+            // }
+            float fadePercent = 1 - Mathf.Pow(
+                ((playerYPos - trackTwoFadeOutStartY) / (trackTwoFadeOutEndY - trackTwoFadeOutStartY)), 2
+            );
+            trackTwo.volume = trackTwoStartVolume * fadePercent;
+            // trackOneFilter.cutoffFrequency = trackOneStartFilter * fadePercent;
+        }
+
+        // Fade in track 3
+        if(playerYPos <= trackThreeFadeInStartY && playerYPos >= trackThreeFadeInEndY) {
+
+            float fadePercent = Mathf.Pow(
+                ((playerYPos - trackThreeFadeInStartY) / (trackThreeFadeInEndY - trackThreeFadeInStartY)), 2
+            );
+            trackThree.volume = trackThreeStartVolume * fadePercent;
         }
     }
 }
--- Assets/Scripts/SkipController.cs
+++ Assets/Scripts/SkipController.cs
@@ -17,34 +17,71 @@ public class SkipController : MonoBehaviour
     public bool developmentMode; // Ignores track skipping.
     private AudioSource startTrack;
     public float fadeInSpeed = 0.25f;
+    private bool fadeInComplete = false;
+    private float trackOneMaxVolume;
+    private float trackTwoMaxVolume;
+    private float trackThreeMaxVolume;
+    private float startTrackMaxVolume;
 
     void Start()
     {
+        // Remember the "max" volumes of each music when it's on.
+        trackOneMaxVolume = trackOneMusic.volume;
+        trackTwoMaxVolume = trackTwoMusic.volume;
+        trackThreeMaxVolume = trackThreeMusic.volume;
+
+        // Start all volumes at 0.
+        trackOneMusic.volume = 0;
+        trackTwoMusic.volume = 0;
+        trackThreeMusic.volume = 0;
+
         int track;
         if(!developmentMode) {
             TrackSkipper skipper = GameObject.Find("TrackSkipper").GetComponent<TrackSkipper>();
             track = skipper.GetTrackSkip();
         } else {
-            track = 3;
+            track = 2;
         }
         
         if(track == 1) {
             if(!developmentMode) Instantiate(player, trackOneStart.position, Quaternion.AngleAxis(90, Vector3.up));
             startTrack = trackOneMusic;
+            startTrackMaxVolume = trackOneMaxVolume;
+            
             trackOneTitleTrigger.MakeFirstTrack();
         } else if(track == 2) {
             if(!developmentMode) Instantiate(player, trackTwoStart.position, Quaternion.AngleAxis(90, Vector3.up));
             startTrack = trackTwoMusic;
+            startTrackMaxVolume = trackTwoMaxVolume;
+            
             trackTwoTitleTrigger.MakeFirstTrack();
         } else {
             if(!developmentMode) Instantiate(player, trackThreeStart.position, Quaternion.AngleAxis(90, Vector3.up));
             startTrack = trackThreeMusic;
+            startTrackMaxVolume = trackThreeMaxVolume;
+            
             trackThreeTitleTrigger.MakeFirstTrack();
         }
     }
 
     void Update() {
         // Fade in the music.
-        startTrack.volume += fadeInSpeed * Time.deltaTime;
+        if(!fadeInComplete && startTrack.volume < startTrackMaxVolume) {
+            startTrack.volume += fadeInSpeed * Time.deltaTime;
+        } else {
+            if(!fadeInComplete) fadeInComplete = true;
+        }
+    }
+
+    public float TrackOneMaxVolume() {
+        return trackOneMaxVolume;
+    }
+
+    public float TrackTwoMaxVolume() {
+        return trackTwoMaxVolume;
+    }
+
+    public float TrackThreeMaxVolume() {
+        return trackThreeMaxVolume;
     }
 }
